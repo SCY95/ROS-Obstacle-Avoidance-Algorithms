abfb5391efa22bd6daa7e954e27c8d6589faaf0d
Salih Can YILMAZ <canyilmaz999@gmail.com>
2020-03-22 20:37:13 +0300

    init commit

---
+++ ecs_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/scy95/ecs_ws/src
+# Build directory: /home/scy95/ecs_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(two-wheeled-robot-motion-planning)
+subdirs(m2wr_description)
---
+++ ecs_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/scy95/ecs_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/scy95/ecs_ws/devel;/home/scy95/mybot_ws/devel;/home/scy95/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/scy95/ecs_ws/devel;/home/scy95/mybot_ws/devel;/home/scy95/catkin_ws/devel;/opt/ros/kinetic" != "" else []
